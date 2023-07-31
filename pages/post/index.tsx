e23e8e98b696874433ec786bbbc9ca9626ca1f7a
soon0180 <https://github.com/soon0180>
2023-07-31 19:28:12 +0900

    update : post page connet btn

---
+++ pages/post/index.tsx
@@ -0,0 +1,68 @@
+import BaseLayout from '@/src/layout/BaseLayout'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { ReactElement } from 'react'
+import PostItem from './PostCard'
+import styled from 'styled-components'
+const inter = Inter({ subsets: ['latin'] })
+
+const PostBenner = styled.div`
+    display: flex;
+    margin: 0 auto;
+    margin-bottom: 128px;
+    & > img {
+        width: 500px;
+        height: 360px;
+        background-color: gray;
+        margin-right: 78px;
+    }
+
+    & > div {
+        & > p {
+            font-size: 32px;
+            font-weight: bold;
+            margin-bottom: 78px;
+        }
+        & > h1 {
+            font-size: 32px;
+            font-weight: bold;
+        }
+        & > h3 {
+            font-size: 18px;
+            font-weight: bold;
+        }
+    }
+`
+
+const Post = () => {
+    return (
+        <>
+            <Head>
+                <title>리뷰 - 메인페이지</title>
+            </Head>
+
+            <main className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
+                <PostBenner>
+                    <img alt="" />
+                    <div>
+                        <p>BlockChain</p>
+                        <h1>아무글자나 넣자</h1>
+                        <h3>아무글자나 넣자</h3>
+                    </div>
+                </PostBenner>
+                <div className="bg-white">
+                    <div className="w-full">
+                        <div className="w-full flex">
+                            <PostItem></PostItem>
+                            <PostItem></PostItem>
+                            <PostItem></PostItem>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+Post.getLayout = (page: ReactElement) => <BaseLayout>{page}</BaseLayout>
+export default Post
--- src/layout/BaseLayout/Header/index.tsx
+++ src/layout/BaseLayout/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SetStateAction } from 'react'
 import WalletConnectButton from './WalletConnectButton'
+import styled from 'styled-components'
 import UserBox from './UserBox'
 
 interface HeaderProps {
@@ -9,17 +10,66 @@ interface HeaderProps {
     setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+const Title = styled.h1`
+    font-size: 32px;
+    font-weight: bold;
+    margin-top: 13px;
+    margin-bottom: 13px;
+    margin-left: 20px;
+    line-height: 32px;
+`
+const Menu = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    & > li {
+        margin: 20px;
+        font-size: 14px;
+        font-weight: bold;
+    }
+`
+
+const MenuUser = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    & > .user-box-warp {
+        padding: 11px 33px;
+        height: 40px;
+        background-color: #ca2828;
+        border-radius: 6px;
+        font-size: 15px;
+        color: white;
+        font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
 const Header: React.FC<HeaderProps> = ({ isConnected, isLoading, user, setIsOpenModal }) => {
     return (
-        <div className="flex flex-row justify-between">
-            <h1>logo</h1>
-            <ul>
-                <li>
-                    <WalletConnectButton isConnected={isConnected} setIsOpenModal={setIsOpenModal} />
-                    <UserBox isConnected={isConnected} isLoading={isLoading} user={user} />
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="flex flex-row justify-between relative">
+                <Title>LTL</Title>
+                <Menu>
+                    <li>Posting</li>
+                    <li>DAO</li>
+                    <li>Punding</li>
+                </Menu>
+                <MenuUser>
+                    <li className="user-box-warp">
+                        <WalletConnectButton isConnected={isConnected} setIsOpenModal={setIsOpenModal} />
+                        <UserBox isConnected={isConnected} isLoading={isLoading} user={user} />
+                    </li>
+                </MenuUser>
+            </div>
+            <div className="bg-gray-200 w-full h-6"></div>
+        </>
     )
 }
 
